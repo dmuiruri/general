be57c77996f021965a17e04f3ee0521a127cfb65
dennisn <denonjugush@gmail.com>
2016-11-22 13:15:42 +0200

    added a test file for the matrix implementation

---
+++ matrix/matrix_test.cpp
@@ -0,0 +1,31 @@
+#include "matrix.h"
+#include <iostream>
+#include <assert.h>
+using namespace std;
+
+
+int main(int argc, char **argv){
+
+  Matrix<double> matrix1(10, 10, 10.0);
+  Matrix<double> matrix2(10, 10, 20.0);
+
+  cout << ">> Testing matrix implementation " << endl;
+
+  // Test for addition and overloaded assignment
+  Matrix<double> result = matrix1 + matrix2;
+  result.print_matrix();
+
+  // Test for subtraction and overloaded assignment
+  result = matrix2 - matrix1;
+  result.print_matrix();
+  
+  // Test for Multiplication
+  result = matrix1 * matrix2;
+  result.print_matrix();
+
+  // TODO: Implement other tests
+  
+return 0;
+}
+
+
