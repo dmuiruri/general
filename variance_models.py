37fe9d6ec969c1fe3d59dacc56d6ab8b2cbd2822
dennisn <denonjugush@gmail.com>
2019-09-05 13:08:56 +0300

    computing variance using a single pass approach \- unstable method

--- variance_models.py
+++ variance_models.py
@@ -15,11 +15,21 @@ How can variance be computed in a stable form?
 
 def twopass_var(x):
     """
-    Calculate the variance using a single pass
+    Calculate the variance using a two pass approach
+
+    Two pass alludes to going through the data twice, first to compute
+    the mean then the sum of squared differences.
     """
     x = np.array(x)
-    x_mean = np.mean(x)
-    return (1/len(x)-1) * sum([(i-x_mean)**2 for i in x])
+    x_mean = sum(x)/len(x)
+    return 1/(len(x)-1) * sum([(i-x_mean)**2 for i in x])
+
+def onepass_var(x):
+    """
+    Calculate the variance using a single pass approach
+    """
+    x = np.array(x)
+    return 1/(len(x)-1) * (sum(x**2) - (1/len(x) * sum(x)**2))
 
 if __name__ == '__main__':
     mu = 10e9
@@ -27,4 +37,5 @@ if __name__ == '__main__':
     samples = 1000
     x = np.random.normal(mu, sigma, samples)
     print("Var of vector x using np {}".format(np.var(x)))
-    print("Var of vector x using two pass algorithm: {}".format(singlepass_var(x)))
+    print("Var of vector x using two pass algorithm: {}".format(twopass_var(x)))
+    print("Var of vector x using single pass algorithm: {}".format(onepass_var(x)))
