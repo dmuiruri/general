3c9a0ab48901439b0cb8f2dfb4ddc1b373a6fcb6
dennisn <denonjugush@gmail.com>
2019-09-12 16:13:07 +0300

    Implemented the pvalue computation

--- perm_testing.py
+++ perm_testing.py
@@ -17,17 +17,42 @@ import matplotlib.pyplot as plt
 
 data = pd.read_csv('http://www.cs.helsinki.fi/u/ahonkela/teaching/compstats1/fram.txt', sep='\t')
 
-# print(data.describe())
+print(data.columns)
 
 Nperms = 1000 # No of permutations
-
-# Generate male female masks
-# Imale = (fram['SEX'] == 'male').values
-# Ifemale = (fram['SEX'] == 'female').values
 
 m_mean_age = np.mean(data['AGE'][data['SEX'] == 'male'])
 f_mean_age = np.mean(data['AGE'][data['SEX'] == 'female'])
 
-print("True mean difference {}".format(m_mean_age - f_mean_age))
+true_m_diff = m_mean_age - f_mean_age
 
-# if __name__ == '__main__':
+meandiffs = np.zeros(Nperms)
+
+print("True mean difference {}".format(true_m_diff))
+
+def permute_arrays(s1, s2):
+    """
+    permute two series
+    
+    A helper function to permute/shuffle two series
+    """
+    n = len(s1)
+    s3 = np.random.permutation(s1.append(s2))
+    return s3[0:n], s3[n:]
+    
+def permutation_test(x1, x2):
+    """
+    Run permutations
+    """
+    for i in range(Nperms):
+        s1, s2 = permute_arrays()
+        meandiffs[i] = np.abs(np.mean(s1) - np.mean(s2))
+
+def pvalue(truediff, mean_diffs):
+    """
+    Calculate the p-value based on simulated means
+    """
+    return np.sum((truediff <= mean_diffs) + 1)/(len(meandiffs) + 1)
+
+if __name__ == '__main__':
+    print("P-Value: ", pvalue(true_m_diff, meandiffs))
