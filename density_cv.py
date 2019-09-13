7d0eba17fbcad8d79d775cc18a0f866c8e16d724
dennisn <denonjugush@gmail.com>
2019-09-13 12:50:33 +0300

    Added kernel density estimator

--- density_cv.py
+++ density_cv.py
@@ -17,6 +17,8 @@ import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
 
+d = pd.read_csv('http://www.cs.helsinki.fi/u/ahonkela/teaching/compstats1/toydata.txt').values
+
 def K_gauss(x):
     """
     Generate the Gaussian Kernel for x
@@ -35,4 +37,25 @@ def K_epanechnikov(x):
     """
     return (3 / 4)*(1 - x**2)*(np.abs(x) < 1) 
 
+def kernel_density(t, x, h):
+    """
+    Calculate the kernel density
+
+    x: input array containing obs to be estimated
+    h: width of the kernel
+    """
+    y = np.zeros(len(t))
+    for i in range(len(t)):
+        y[i] = np.mean(K_gauss((t[i] - x)/h))/h
+    return y
+
+if __name__ == '__main__':
+    t = np.linspace(-2, 10, 100)
+    nb = plt.hist(d, 30, normed=True)
+
+    plt.plot(t, kernel_density(t, d, 3.0), label='h=3.0')
+    plt.plot(t, kernel_density(t, d, 1.0), label='h=1.0')
+    plt.plot(t, kernel_density(t, d, 0.3), label='h=0.3')
+    plt.plot(t, kernel_density(t, d, 0.1), label='h=0.1')
+    plt.show()
     
