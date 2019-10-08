ac3310953c141341dc0ee8ef2d1ea0791b5e8d0d
dennisn <denonjugush@gmail.com>
2019-10-08 19:22:24 +0300

    Added a laplacian regression (robust regression)

--- lm_llhood.py
+++ lm_llhood.py
@@ -1,8 +1,16 @@
 #! /usr/bin/env python
 
 """
-A module that demonstrates the use of maximum likelihood towards the
-estimation of linear regression and robust regression
+A module that demonstrates the use of log likelihood towards the
+estimation of linear regression and robust regression. When estimating
+the likelihood, we are estimating for the parameters of the model.
+
+The maximum log-likelihood implementation below gets the maximum by
+MINIMISING the NEGATIVE log-likelihood
+
+A typical linear regression assumes that the estimation errors follow
+a Normal distribution, however, a robust regression can be obtained by
+using an alternative distribution of the error terms.
 
 The Likelihood of a variable in a given distribution => pdf of that given variable
 The log-likelihood of a variable in a given distn => log pdf version of the given dist
@@ -15,7 +23,7 @@ import autograd
 from scipy.optimize import minimize
 from scipy.stats import norm
 
-def linear_reg_logl(coefs, x, y):
+def linear_reg_normlogpdf(coefs, x, y):
     """
     Log likelihood for a linear regression where the error term
     follows a normal distribution see the normal dist pdf with mu=0
@@ -40,8 +48,21 @@ def linear_regression(x, y):
     errors (€), the gradient of the function, which is setup by using
     autograd which performs gradient computations.
     """
-    func = lambda c: -linear_reg_logl(c, np.array(x), np.array(y))
+    func = lambda c: -linear_reg_normlogpdf(c, np.array(x), np.array(y))
     d_func = autograd.grad(func) # gradient func
+    v = minimize(func, np.ones(2), jac=d_func, method='L-BFGS-B')
+    return v
+
+def linear_reg_laplacelogpdf(coefs, x, y):
+    """
+    Create laplace model to estimate the distribution of the error
+    term of a linear regression model
+    """
+    return np.sum(-np.log(2)-np.abs(y - coefs[1] - coefs[0]*x))
+
+def laplace_regression(x, y):
+    func = lambda c: -linear_reg_laplacelogpdf(c, np.array(x), np.array(y))
+    d_func = autograd.grad(func)
     v = minimize(func, np.ones(2), jac=d_func, method='L-BFGS-B')
     return v
 
@@ -51,4 +72,6 @@ if __name__ == '__main__':
     y = np.copy(x); y[8]=0
 
     v_norm = linear_regression(x, y)
-    print(v_norm)
+    print("Normal lr:", v_norm)
+    v_lap = laplace_regression(x, y)
+    print("Laplace lr:", v_lap)
